396be74cdcf2807218139fcb42e6fe84a28cf21e
Haydar Memis <haydarmemiiss@gmail.com>
2025-07-17 14:30:32 +0300

    first commit

---
+++ src/main/java/com/neg/hr/human/resouce/HumanResouceApplication.java
@@ -0,0 +1,13 @@
+package com.neg.hr.human.resouce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResouceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResouceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neg/hr/human/resouce/HumanResouceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neg.hr.human.resouce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResouceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
