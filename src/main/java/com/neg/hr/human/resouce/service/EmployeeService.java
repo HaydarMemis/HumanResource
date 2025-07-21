141ab55cb8fa636cf4a410fe16231e9f2570f3f6
Ayşe <ayse.delen@yandex.com>
2025-07-21 12:05:30 +0300

    Finished LeaveType Service and Controller fixed Employee

--- src/main/java/com/neg/hr/human/resouce/service/EmployeeService.java
+++ src/main/java/com/neg/hr/human/resouce/service/EmployeeService.java
@@ -1,12 +1,4 @@
 package com.neg.hr.human.resouce.service;
 
-import com.neg.hr.human.resouce.entity.Employee;
-import java.util.List;
-
-public interface EmployeeService {
-    Employee save(Employee employee);
-    Employee findById(Long id);
-    List<Employee> findAll();
-    void delete(Long id);
-    Employee update(Long id, Employee employee);
+public class EmployeeService {
 }
--- src/main/java/com/neg/hr/human/resouce/service/LeaveTypeService.java
+++ src/main/java/com/neg/hr/human/resouce/service/LeaveTypeService.java
@@ -1,4 +1,39 @@
 package com.neg.hr.human.resouce.service;
 
-public class LeaveTypeService {
+import com.neg.hr.human.resouce.entity.Employee;
+import com.neg.hr.human.resouce.repository.EmployeeRepository;
+import com.neg.hr.human.resouce.repository.LeaveTypeRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
+public class LeaveTypeServiceImp {
+
+    private final LeaveTypeRepository leaveTypeRepository;
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id " + id));
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee update(Long id, Employee employee) {
+        Employee existing = findById(id);
+        existing.setHireDate(employee.getHireDate());
+        existing.setPerson(employee.getPerson());
+        existing.setCompany(employee.getCompany());
+        existing.setPosition(employee.getPosition());
+        return employeeRepository.save(existing);
+    }
 }
