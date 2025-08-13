4f1a9f10123929c7bc74febbe204535f592ed443
Haydar Memis <haydarmemiiss@gmail.com>
2025-08-13 17:17:57 +0300

    Fix: Moved config package

--- src/main/resources/config/SwaggerConfig.java
+++ src/main/resources/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.neg.technology.human.resource.config;
+package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
