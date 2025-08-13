a40f2c723398029b134394e98dbda6c391f075ce
Buraktalhaa <talha45879@gmail.com>
2025-08-13 14:53:44 +0300

    update LeavePolicy Controller

--- src/main/java/com/neg/technology/human/resource/LeavePolicy/controller/LeavePolicyController.java
+++ src/main/java/com/neg/technology/human/resource/LeavePolicy/controller/LeavePolicyController.java
@@ -99,7 +99,7 @@ public class LeavePolicyController {
     }
 
     @Operation(summary = "Get all leave policies")
-    @GetMapping("/all")
+    @PostMapping("/all")
     public ResponseEntity<LeavePolicyResponseList> getAllLeavePolicies() {
         return leavePolicyService.getAllLeavePolicies();
     }
