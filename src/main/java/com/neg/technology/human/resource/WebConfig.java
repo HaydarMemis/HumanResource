85285ae282926fa6d4a639db61859c36f72e4969
Haydar Memis <haydarmemiiss@gmail.com>
2025-08-26 15:17:27 +0300

    added web config

--- src/main/java/com/neg/technology/human/resource/WebConfig.java
+++ src/main/java/com/neg/technology/human/resource/WebConfig.java
@@ -1,4 +1,4 @@
-package com.neg.technology.human.resource.config;
+package com.neg.technology.human.resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
